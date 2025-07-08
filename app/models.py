d6e5050cce814c3ba755c38ea2c4034899bd16b1
abhinavmabhi <dexmarketing192018@gmail.com>
2025-07-08 12:23:28 +0530

    push

--- app/models.py
+++ app/models.py
@@ -1,9 +1,6 @@
 from django.db import models
-
 # Create your models here.
 
-
-from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -11,20 +8,16 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-class SubCategory(models.Model):
-    category_obj = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='subcategories')
-    Sub_category = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f"{self.category_obj.name} - {self.name}"
 
 class Product(models.Model):
-    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE, related_name='products_category')
+    category_obj = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category_obj')
+    is_18k_gold=models.BooleanField(default=False)
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='products/')
-    in_stock = models.BooleanField(default=True)
+    stock = models.PositiveIntegerField(default=0)
+    is_available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -33,16 +26,16 @@ class Product(models.Model):
 class CartItem(models.Model):
 
     session_id = models.CharField(max_length=100) 
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_obj')
     quantity = models.PositiveIntegerField(default=1)
     added_at = models.DateTimeField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together= ('session_id', 'product')
 
     def __str__(self):
-        return f"{self.quantity}x {self.product.name}"
-
-class WishlistItem(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    added_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.product.name
+        return f"{self.quantity} x {self.product.name}"
+    
+    def total_price(self):
+        return self.quantity*self.product.price
